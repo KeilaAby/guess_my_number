ad8271019ada90df1261c0d4205ead6b56ce7d0d
Christian <seorefchristian@gmail.com>
2025-03-23 21:15:17 +0300

    more script for the game

--- starter/script.js
+++ starter/script.js
@@ -23,11 +23,35 @@ Ex d'API WEB : Timers, Fetch
 
 //HANDLING CLICK EVENT
 /* Un evenement est ue chose quis se passe sur notre page web (click, hover, focus, ) */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number !';
-    //J'ai ajouté un commentaires ici pour tests
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number !';
+    // console.log(document.querySelector('body'));
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game !';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game !';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });

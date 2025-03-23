eb0deffb4a4057d49699e3cc00dd5c52f842b5fc
Christian <seorefchristian@gmail.com>
2025-03-23 20:10:27 +0300

    new script

--- starter/script.js
+++ starter/script.js
@@ -12,11 +12,21 @@ Ex d'API WEB : Timers, Fetch
 */
 
 //SELECTING AND MANIPULATING ELEMENTS
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number !';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number !';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent= 10;
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+//HANDLING CLICK EVENT
+/* Un evenement est ue chose quis se passe sur notre page web (click, hover, focus, ) */
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number !';
+  }
+});
